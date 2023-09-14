ed5b3ef03fa2fac1f9a3238a39035002d1425ea7
Milan Bhojani <132556936+Rutvik3092@users.noreply.github.com>
2023-09-14 21:53:07 +0530

    project upload

---
+++ server.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import path from 'path';
+import uiRoute from './ui/ui.route';
+import pageRoute from './page/page.route';
+import assetRoute from './assets/assets.route';
+import projectRoute from './project/project.route';
+import renderHtml from './render/render.controller';
+//Initialize App
+const app = express();
+app.use(express.json());
+const corsOptions = {
+  origin: function (origin, callback) {
+    callback(null, true);
+  },
+};
+
+corsOptions.credentials = true;
+app.use(cors(corsOptions));
+
+//HTML and Static file
+app.use('/resources', express.static(path.join(__dirname, 'public')));
+app.set('views', `views`);
+app.set('view engine', 'hbs');
+
+mongoose.set('strictQuery', false);
+const mongoUri = 'mongodb+srv://milan:milan@cluster0.mmmeqva.mongodb.net/';
+mongoose.connect(
+  mongoUri,
+  {
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+).then(()=>{
+  console.log("Dtabase connected");
+}).catch((err)=>{
+  console.log(err);
+})
+
+app.use('/api/projects', projectRoute);
+app.use('/api/pages', pageRoute);
+app.use('/api/assets', assetRoute);
+app.use('/api/', uiRoute);
+app.get('/:pageId?', renderHtml);
+
+const PORT = process.env.APP_PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
